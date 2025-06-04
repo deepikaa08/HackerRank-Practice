e2bff6c0431e9d96539cbefd1157f4d85fa3456a
Deepikaa Gopalakannan <147126243+deepikaa08@users.noreply.github.com>
2025-06-04 12:03:54 +0530

    Create Generics.java

---
+++ Generics.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Elements{
+    public static <E> void printArray(E[] array){
+        for(E item : array){
+            System.out.println(item);
+        }
+    }
+}
+public class Generics {
+
+    public static void main(String[] args) {
+        Elements e = new Elements();
+        Integer[] nums = {1, 2, 3};
+        String[] words = {"Hello", "World"};
+        e.printArray(nums);
+        e.printArray(words);
+    }
+}
