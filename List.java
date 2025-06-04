8268ccce114996477159027e00e41d4fd7b98fff
Deepikaa Gopalakannan <147126243+deepikaa08@users.noreply.github.com>
2025-06-04 10:44:26 +0530

    Created List

---
+++ List.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class List {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++)
+            list.add(scanner.nextInt());
+            
+        int queries = scanner.nextInt();
+        for(int i = 0; i < queries; i++) {
+            String query = scanner.next();
+            
+            int index = scanner.nextInt();
+            if (query.equals("Insert")){
+                int newValue = scanner.nextInt();
+                list.add(index, newValue); 
+            }
+            else
+                list.remove(index);
+        }
+        
+        scanner.close();
+        
+        for(Integer num : list)
+            System.out.print(num + " ");
+    }
+}
